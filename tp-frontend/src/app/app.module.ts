911d6f4dc3ff95d0e30ebc2f361ec51f8f5de434
andresbertone <andresbertone97@gmail.com>
2021-11-24 16:54:44 -0300

    Feature: Se agrega pantalla de inicio, productos y contacto.

---
+++ tp-frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ProductCardComponent,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
