60e7593d349362e5f2d73b34c35b11ceded31197
andresbertone <andresbertone97@gmail.com>
2021-11-26 16:30:59 -0300

    Se renombra la carpeta

---
+++ frontend/src/app/models/product-model.ts
@@ -0,0 +1,12 @@
+export interface ProductModel {
+
+    id: number;
+    name: string;
+    description: string;
+    image: string;
+    price: number;
+    quotas: number;
+    stock: number;
+    brand: string;
+
+}
