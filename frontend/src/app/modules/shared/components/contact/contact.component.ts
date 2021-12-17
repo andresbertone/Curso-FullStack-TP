dd8b24361af5bbc6c09cdf5769b05e11ee6fb445
andresbertone <andresbertone97@gmail.com>
2021-12-17 20:37:44 -0300

    Fix: Corrección al formulario de contacto

--- frontend/src/app/modules/shared/components/contact/contact.component.ts
+++ frontend/src/app/modules/shared/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SnackbarService } from '@app/snackbar-service/snackbar.service';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contact: any = {};
+
+  constructor( private snackbarService: SnackbarService ) { }
 
   ngOnInit(): void {
   }
+
+  send() {
+    if ( this.isValid() ) {
+      this.snackbarService.openSnackbar("Mensaje enviado", "Ok");
+      this.contact = {};
+    }
+  };
+
+  isValid() {
+    if ( this.contact.firstName && this.contact.surname && this.contact.email && this.contact.message ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
 }
